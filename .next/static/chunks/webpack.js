d624014d206c7ef39977ae8ae787d7ab33ba7800
prasadc01 <prasadc@edreamz.in>
2022-10-14 18:11:28 +0530

    make it better

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "57ecf9408b98b073"; }
+/******/ 		__webpack_require__.h = function() { return "b04f53436cecb5e0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
